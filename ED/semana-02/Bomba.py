46e315a50225f480f629aa29f1f919f4b847a112
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-02-21 14:40:20 -0300

    full commit

---
+++ ED/semana-02/Bomba.py
@@ -0,0 +1,59 @@
+class Visor:
+    def __init__(self, preço_do_combustivel, nome_combustivel):
+        self.nome_combustivel = nome_combustivel
+        self.preço_combustivel = preço_do_combustivel
+        self.quant_litros = 0.0
+        self.valor_a_pagar = 0.0
+
+    def reset(self):
+        pass
+
+    def __str__(self):
+        return f'''
+    Combustivel: {self.nome_combustivel}
+    Preço: {self.preço_combustivel} 
+    Quantidade de Litros: {self.quant_litros}     
+    Total a pagar: {self.valor_a_pagar} 
+    '''
+
+
+class BombaDeCombustivel:
+    ALCOOL = 1
+    GASOLINA = 2
+    def __init__(self, preço_da_gasolina:float, preço_do_alcool:float):
+        self.preço_gasolina = preço_da_gasolina
+        self.preço_alcool = preço_do_alcool
+        self.ligada = True
+
+        self.visor_alcool = Visor(self.preço_alcool, 'ALCOOL')
+        self.visor_gasolina = Visor(self.preço_gasolina, 'GASOLINA')
+
+    def abastercer(self):
+        pass
+
+    def zerarBomba(self, tipo_combustivel):
+        if tipo_combustivel == BombaDeCombustivel.GASOLINA:
+            self.visor_gasolina.reset()
+        # elif tipo_combustivel
+
+
+
+    def alterarPreçoGasolina(self, novoPreço:float):
+        if novoPreço > 0:
+            self.preço_gasolina = novoPreço
+
+    def alterarPreçoAlcool(self, novoPreço:float):
+        if novoPreço > 0:
+            self.preço_alcool = novoPreço
+
+    def mostrarVisor(self):
+        pass
+
+    def ligar(self):
+        self.ligada = True
+
+    def desligar(self):
+        self.ligada = False
+
+
+
--- ED/semana-02/main.py
+++ ED/semana-02/main.py
@@ -1,1 +1,20 @@
-print("ola mundo")
+from Bomba import BombaDeCombustivel, Visor
+
+# programa principal
+b1 = BombaDeCombustivel(6.50, 5.20)
+print(b1.preço_gasolina)
+print(b1.__dict__)
+b1.preço_gasolina = 6.10
+print(b1.__dict__)
+
+b1.zerarBomba(BombaDeCombustivel.GASOLINA)
+
+b1.desligar()
+if b1.ligada == False:
+    print('****** A bomba foi desligada neste momento ******')
+
+visor_alcool = Visor(5.20, 'ALCOOL')
+print(visor_alcool)
+
+visor_gasolina = Visor(5.20, 'GASOLINA')
+print(visor_gasolina)
