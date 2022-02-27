5058f363d5987cf8e8da3b6bc7d5a0a455821759
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-02-27 15:23:19 -0300

    calculator commit

--- LS/semana-02/atividade/index.js
+++ LS/semana-02/atividade/index.js
@@ -1,3 +1,7 @@
-import area_do_circulo from "./lib.js";
+import { area_do_circulo, calculadora } from "./lib.js";
 
+console.log("Exercício área do círculo:")
 console.log(area_do_circulo(10))
+
+console.log("Exercício calculadora:")
+console.log(calculadora(1,2,"/"))
--- LS/semana-02/atividade/lib.js
+++ LS/semana-02/atividade/lib.js
@@ -1,8 +1,44 @@
 function area_do_circulo(raio){
 
-    const pi = 3.141592653589793
-    let area = pi * (raio**2)
-    return `Dado o raio ${raio}, obtemos a área ${area}`
+    const pi = 3.141592653589793;
+    let area = pi * (raio**2);
+    return `Dado o raio ${raio}, obtemos a área ${area}`;
 }
 
-export default area_do_circulo
+function calculadora(val1, val2, ope){
+
+    let resultado = 0;
+    switch (ope) {
+
+        case "+":
+            resultado = val1 + val2;
+            break;
+
+        case "-":
+            resultado = val1 - val2;
+            break;
+
+        case "*":
+            resultado = val1 * val2;
+            break;
+
+        case "/":
+            resultado = val1 / val2;
+            break;
+
+        default:
+            resultado = "Operação Inválida";
+        
+    }  
+    if (resultado == "Operação Inválida"){
+
+        return resultado;
+
+    } else{
+
+        return `${val1} ${ope} ${val2} = ${resultado}`;
+
+    }
+}
+
+export { area_do_circulo, calculadora }
