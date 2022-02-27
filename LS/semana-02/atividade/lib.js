1594edec0a18f09a212da77d6e8ab9ed50db33e1
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-02-27 11:11:47 -0300

    Update lib.js

--- LS/semana-02/atividade/lib.js
+++ LS/semana-02/atividade/lib.js
@@ -2,7 +2,7 @@ function area_do_circulo(raio){
 
     const pi = 3.141592653589793
     let area = pi * (raio**2)
-    return area
+    return `Dado o raio ${raio}, obtemos a área ${area}`
 }
 
 export default area_do_circulo
