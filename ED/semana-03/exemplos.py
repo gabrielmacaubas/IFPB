36133ad5d3dceeded3d77785a2f6170d2bc5541b
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-02-28 14:46:41 -0300

    Update exemplos.py

--- ED/semana-03/exemplos.py
+++ ED/semana-03/exemplos.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    
     def __init__(self, nome, idade):
         self._nome = nome
         self._idade = idade
     
-    def get_idade(self):
+    @property
+    def idade(self):
         return self._idade
     
-    def set_idade(self, nova_idade):
-        self._idade = nova_idade
+    @idade.setter
+    def idade(self, nova_idade):
+        if nova_idade >= 0:
+            self._idade = nova_idade
     
 aluno = Pessoa('Carlos', 19)
 print('Nome do aluno:', aluno._nome)
-aluno.set_idade(-3)
-print('Idade do aluno:', aluno.get_idade())
+aluno.idade = (-3)
+print('Idade do aluno:', aluno.idade)
