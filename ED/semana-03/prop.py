f7228e5c97ce2bd4443271704b72674c915ba3d0
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-03-07 14:43:38 -0300

    Create prop.py

---
+++ ED/semana-03/prop.py
@@ -0,0 +1,25 @@
+class Relogio:
+
+    def __init__(self, seg):
+        self.seg = seg
+    
+    @property
+    def minutos(self):
+        print('obtendo valor')
+        return self.seg // 60
+
+    @minutos.setter
+    def minutos(self, valor):
+        print('setando valor')
+        self.seg = valor * 60
+
+    @property
+    def horas(self):
+        return self.minutos // 60
+
+    @horas.setter
+    def horas(self, valor):
+        print('cascata')
+        self.minutos = valor * 60
+
+
