5d8b8bced4bbcf43f4ffa786ff4fb012cb6a9afa
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-03-08 08:44:05 -0300

    OOP exercise commit

--- ED/semana-03/ex01.py
+++ ED/semana-03/ex01.py
@@ -1,12 +1,40 @@
 class Data:
-    def __init__(self, dia=0, mes=0, ano=0):
-        self.__dia = dia
-        self.__mes = mes
-        self.__ano = ano
+    def __init__(self):
+        self.__dia = int()
+        self.__mes = int()
+        self.__ano = int()
+
+    def __str__(self):
+        return f'{self.__dia}/{self.__mes}/{self.__ano}'
 
     @property
     def dia(self):
         return self.__dia
 
-    #@property
-    #def dia(self):
+    @property
+    def mes(self):
+        return self.__mes
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @dia.setter
+    def dia(self, d):
+        self.__dia = d
+
+    @mes.setter
+    def mes(self, m):
+        self.__mes = m
+
+    @ano.setter
+    def ano(self, a):
+        self.__ano = a
+
+# Programa Principal
+data = Data()
+data.dia = 8
+data.mes = 3
+data.ano = 2022
+
+print(data)
