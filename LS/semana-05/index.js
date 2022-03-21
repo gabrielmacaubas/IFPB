893ff8413b355d0f572bb3166855c26206380d29
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-03-21 16:03:02 -0300

    Create index.js

---
+++ LS/semana-05/index.js
@@ -0,0 +1,56 @@
+// Person
+// Name:
+// Email: john@email.com
+
+// const name = 'John';
+// const email = 'john@email.com';
+
+// const [name, email] = ['John', 'john@emai.com'];
+
+// const person = ['John', 'john@email.com'];
+// console.log(person[0]);
+
+// JSON (JavaScript Object Notation)
+// const person = {"name": 'John', email: 'john@email.com'};
+// const person = {name: 'John', email: 'john@email.com'};
+// console.log(person.name);
+
+
+// const people = [
+//   ['Alice', 'alice@email.com'],
+//   ['Bob', 'bob@email.com'],
+// ];
+// people[1][1];
+
+//const people = [
+//   {name: 'Alice', email: 'alice@email.com'},
+//   {name: 'Bob', email: 'bob@email.com'}
+// ];
+// console.log(people[1].email);
+
+// const people = {
+//   student1: {name: 'Alice', email: 'alice@email.com'},
+//   teacher1: {name: 'Bob', email: 'bob@email.com'}
+// };
+
+// people['teacher1']['name'];
+// people.teacher1.name;
+
+
+// const person = {name: 'John', email: 'john@email.com'};
+// person.phone = '083911112222';
+// person.name = 'John Doe';
+// console.log(person);
+
+// const person = {name: 'John', email: 'john@email.com'};
+// const student = {...person, id: '123'};
+
+// const [name, email] = ['John', 'john@email.com'];
+// const {name, email} = {name: 'John', email: 'john@email.com'};
+// console.log(name);
+
+const name = 'John';
+const email = 'john@email.com';
+
+// const person = {name: name, email: email};
+const person = {name, email};
