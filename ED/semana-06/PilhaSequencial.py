162b5000a26e535989b928eb1123fa1f873d5385
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-03-28 12:11:52 -0300

    Create PilhaSequencial.py

---
+++ ED/semana-06/PilhaSequencial.py
@@ -0,0 +1,45 @@
+class PilhaException(Exception):
+    def __init__(self, mensagem):
+        super().__init__(mensagem)
+
+class PilhaSequencial:
+    def __init__(self):
+        self.__dados = []
+    
+    def vazia(self):
+        return len(self.__dados) == 0
+    
+    def tamanho (self):
+        return len(self.__dados)
+    
+    def topo(self):
+        if self.vazia():
+            raise PilhaException('A pilha está vazia')
+
+        return self.__dados[0]
+
+    def inserir(self, dado):
+        self.__dados.insert(0,dado)
+
+    def remover(self):
+        if self.vazia():
+            raise PilhaException('A pilha está vazia')
+
+        return self.__dados.pop(0)
+
+    def __str__(self):
+        return self.__dados.__str__()
+
+    def imprimir(self):
+        print(self.__str__())
+
+if __name__ == '__main__':
+    p = PilhaSequencial()
+
+    for i in range(1,6):
+        p.inserir(i * 10)
+
+    print(p)
+
+    p.remover()
+    print(p)
