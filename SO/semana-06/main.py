b842b1bb84317f2ac3d11bfa34e40e8e2e9139ea
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-04-01 10:18:18 -0300

    Update main.py

--- SO/semana-06/main.py
+++ SO/semana-06/main.py
@@ -1,4 +1,25 @@
-import threading
+from threading import Thread
+from time import sleep
 
-def f1():
-    print()
+n = 10
+
+
+def f1(nome_thread):
+    global n
+    sleep(0.1)
+    n += 1
+    print(nome_thread, n)
+
+
+def f2(nome_thread):
+    global n
+    n += 1
+    print(nome_thread, n)
+
+
+t1 = Thread(target=f1, args=(1,))
+t1.start()
+t2 = Thread(target=f2, args=(2,))
+t2.start()
+
+print('Thread main')
