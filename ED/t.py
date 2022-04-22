ce53a6ee13acfc51a7b0a69d313abc2021fac622
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-04-22 14:49:27 -0300

    class commit

--- ED/t.py
+++ ED/t.py
@@ -1,1 +1,32 @@
-print("digite: [", input(), "]")
+"""
+while True:
+    print("a")
+    continuar = True
+
+    if continuar:
+        break
+"""
+
+numero1 = 27
+numero2 = 0
+
+if numero1:
+    print("True")
+else:
+    print("False")
+
+if numero2:
+    print("True")
+else:
+    print("False")
+
+"""
+while jogador1 > 5 and jogador2 > 5:
+    ganhador = input("Digite o ganhador")
+    if ganhador == "jogador1":
+        jogador1 += 1
+    else:
+        jogador2 += 1
+
+print("Fim do programa")
+"""
