8fb5824c73f560bedd40d8615b4098af4ddce001
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-05-10 15:21:37 -0300

    Add files via upload

--- SO/semana-12/so-jantar-filosofos-master/src/DiningPhilosophers.java
+++ SO/semana-12/so-jantar-filosofos-master/src/DiningPhilosophers.java
@@ -4,12 +4,40 @@
  * Criada em: 30/08/2017
  */
 
-public class DiningPhilosophers {
+// public class DiningPhilosophers {
 
-    //V1 - Dará deadlock rapidamente, pois todos tentarão pegar o garfo da esquerda.
+//     //V1 - Dará deadlock rapidamente, pois todos tentarão pegar o garfo da esquerda.
+//     public static void main(String[] args) throws Exception {
+
+//         Philosopher[] philosophers = new Philosopher[5];
+//         Object[] forks = new Object[philosophers.length];
+
+//         for (int i = 0; i < forks.length; i++) {
+//             forks[i] = new Object();
+//         }
+
+//         for (int i = 0; i < philosophers.length; i++) {
+//             Object leftFork = forks[i];
+//             Object rightFork = forks[(i + 1) % forks.length];
+
+//             philosophers[i] = new Philosopher(leftFork, rightFork);
+
+//             Thread t
+//                     = new Thread(philosophers[i], "Philosopher " + (i + 1));
+//             t.start();
+//         }
+//     }
+
+    //V2
+    //TODO-2 Descomente o código abaixo e comente o main V1 acima
+    /*
+     * Com esse novo código, o último filósofo irá tentar pegar o garfo da direita, e não o da esquerda,
+     * possibilitando assim que não ocorra uma espera circular, não causando deadlock (não fechará mais o círculo)
+     */
+
     public static void main(String[] args) throws Exception {
 
-        Philosopher[] philosophers = new Philosopher[5];
+        final Philosopher[] philosophers = new Philosopher[5];
         Object[] forks = new Object[philosophers.length];
 
         for (int i = 0; i < forks.length; i++) {
@@ -20,45 +48,17 @@ public class DiningPhilosophers {
             Object leftFork = forks[i];
             Object rightFork = forks[(i + 1) % forks.length];
 
-            philosophers[i] = new Philosopher(leftFork, rightFork);
+            if (i == philosophers.length - 1) {
+
+                // The last philosopher picks up the right fork first
+                philosophers[i] = new Philosopher(rightFork, leftFork);
+            } else {
+                philosophers[i] = new Philosopher(leftFork, rightFork);
+            }
 
             Thread t
                     = new Thread(philosophers[i], "Philosopher " + (i + 1));
             t.start();
         }
     }
-
-    //V2
-    //TODO-2 Descomente o código abaixo e comente o main V1 acima
-    /*
-     * Com esse novo código, o último filósofo irá tentar pegar o garfo da direita, e não o da esquerda,
-     * possibilitando assim que não ocorra uma espera circular, não causando deadlock (não fechará mais o círculo)
-     */
-
-//        public static void main(String[] args) throws Exception {
-//
-//            final Philosopher[] philosophers = new Philosopher[5];
-//            Object[] forks = new Object[philosophers.length];
-//
-//            for (int i = 0; i < forks.length; i++) {
-//                forks[i] = new Object();
-//            }
-//
-//            for (int i = 0; i < philosophers.length; i++) {
-//                Object leftFork = forks[i];
-//                Object rightFork = forks[(i + 1) % forks.length];
-//
-//                if (i == philosophers.length - 1) {
-//
-//                    // The last philosopher picks up the right fork first
-//                    philosophers[i] = new Philosopher(rightFork, leftFork);
-//                } else {
-//                    philosophers[i] = new Philosopher(leftFork, rightFork);
-//                }
-//
-//                Thread t
-//                        = new Thread(philosophers[i], "Philosopher " + (i + 1));
-//                t.start();
-//            }
-//        }
 }
