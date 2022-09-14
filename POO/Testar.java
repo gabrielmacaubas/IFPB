9cd326b8a9ae45392bc83cdbc585179f1c12a4b2
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-09-14 11:00:27 -0300

    exercise commit

---
+++ POO/Testar.java
@@ -0,0 +1,6 @@
+public class Testar {
+    public static void main(String[] args) {
+        String c=1 + 2 + "3";
+        System.out.println(c);
+    }
+}
--- POO/semana-03/AlunoFlex.java
+++ POO/semana-03/AlunoFlex.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public class AlunoFlex {
     String matricula;
 	String nome;
