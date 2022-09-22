e25563988ad8ef48f022c0c00c34667ccac27479
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-09-22 10:46:45 -0300

    exercise commit

--- POO/semana-05/Estacionamento.java
+++ POO/semana-05/Estacionamento.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estacionamento {
     private String[] placas;
 
@@ -9,12 +12,44 @@ public class Estacionamento {
         this.placas[vaga - 1] = placa;
     }
 
-    /*public void sair(vaga) {
-        this.placas[]
+    public void sair(int vaga) {
+        this.placas[vaga - 1] = null;
     }
-    */
 
-    public String[] mostrarPlacas() {
+    
+    public int consultarPlaca(String placa) {
+        for(int i = 0; i < this.placas.length; i++) {
+            if(this.placas[i] == placa) {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public String consultarVaga(int vaga) {
+        return this.placas[vaga - 1];
+    }
+
+    public void transferir(int vaga1, int vaga2) {
+        this.placas[vaga2 - 1] = this.placas[vaga1 - 1];
+        this.placas[vaga1 - 1] = null;
+    }
+    
+    public Object[] consultarVagasLivres() {
+        ArrayList<Integer> vagasLivresAL = new ArrayList<>();
+
+        for(int i = 0; i < this.placas.length; i++) {
+            if(this.placas[i] == null) {
+                vagasLivresAL.add(i + 1);
+            }
+        }
+
+        Object[] vagasLivres = vagasLivresAL.toArray();
+        return vagasLivres;
+    }
+
+    public String[] consultarGeral() {
         return this.placas;
     }
 }
--- POO/semana-05/TesteEstacionamento.java
+++ POO/semana-05/TesteEstacionamento.java
@@ -1,11 +1,17 @@
 public class TesteEstacionamento {
     public static void main(String[] args) {
         Estacionamento e1 = new Estacionamento(10);
-        e1.entrar("SPFC", 4);
-        String[] placas = e1.mostrarPlacas();
+        e1.entrar("SPFC", 10);
+        e1.transferir(10, 3);
 
+        String[] placas = e1.consultarGeral();
+
+        /* 
         for(String placa : placas) {
             System.out.println(placa);
         }
+*/
+
+        e1.consultarVagasLivres();
     }
 }
